e5e035db00f3b6d0b98faf6dff6e1595411cf619
tsdavs <tsdavs@gmail.com>
2019-02-20 23:23:01 +1100

    creat glut window

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include "window.hpp"
+#include "GL/freeglut.h"
+#include "GL/gl.h"
 
 int main(int argc, char* argv[]) 
 {
 	try
 	{
+		glutInit(&argc, argv);
+
+		glutInitDisplayMode(GLUT_SINGLE);
+
+		glutInitWindowSize(500, 500);
+
+		glutInitWindowPosition(100, 100);
+
+		glutCreateWindow("arduino_mpu6050");
+
 		Serial serial;
 
 	} catch (boost::system::system_error& e) 
